f28751475dfa096e79a0fc62a36dfb631deacd7b
Jaya Suriya <jayasuriya@revolite.io>
2020-01-08 11:36:38 +0530

    outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppMainComponent } from './app-main/app-main.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppMainComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
