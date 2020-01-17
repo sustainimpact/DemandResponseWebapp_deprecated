978971e2a9882b3c649be4cec6af172b84f5ddd9
Jaya Suriya <jayasuriya@revolite.io>
2020-01-17 12:40:27 +0530

    ruter

--- src/app/app-main/app-main-routing.module.ts
+++ src/app/app-main/app-main-routing.module.ts
@@ -6,14 +6,30 @@ import { SelectEventCustomersComponent } from '../createevent/select-event-custo
 import {CreateEventDetailsComponent} from '../CreateEvent/create-event-details/create-event-details.component';
 import {CreateEventHomeComponent} from '../CreateEvent/create-event-home/create-event-home.component';
 import { PopUpComponent } from '../CreateEvent/pop-up/pop-up.component';
+import { PublishEventModalComponent } from '../modals/publish-event-modal/publish-event-modal.component';
+import { DownloadReportsModalComponent } from '../modals/download-reports-modal/download-reports-modal.component';
+import { AcceptBidModalComponent } from '../modals/accept-bid-modal/accept-bid-modal.component';
+import { EventsOverviewComponent } from '../createevent/events-overview/events-overview.component';
+import { EventSetCustomersComponent } from '../createevent/event-set-customers/event-set-customers.component';
+import { VersionHistoryComponent } from '../createevent/version-history/version-history.component';
+import { RejectBidModalComponent } from '../Modals/reject-bid-modal/reject-bid-modal.component';
+
 const routes: Routes = [
   {path: '', component: AppMainComponent, 
     children : [
-      { path: 'selecteventcustomers', component: SelectEventCustomersComponent,data: { breadcrumb: 'Select Event Customers '} },
-      { path: 'createeventdetails', component: CreateEventDetailsComponent, data: { breadcrumb: 'Create Event Details'} },
+      { path: '', component: CreateEventDetailsComponent, data: { breadcrumb: 'Create Event Details'}},
+      { path: 'selecteventcustomers', component: SelectEventCustomersComponent , data: { breadcrumb: 'Select Event Customers '}},
+      { path: 'createeventdetails', component: CreateEventDetailsComponent , data: { breadcrumb: 'Create Event Details'}},
       { path: 'createevent', component: CreateEventHomeComponent , data: { breadcrumb: 'Create Event Home'}},
       { path: 'popup', component: PopUpComponent , data: { breadcrumb: 'PopUp'}},
-      { path: '', component: CreateEventDetailsComponent, data: { breadcrumb: 'Create Event Details'} },
+      { path: 'publisheventmodal', component: PublishEventModalComponent , data: { breadcrumb: 'Publish Event Modal'}},
+      { path: 'downloadreportsmodal', component: DownloadReportsModalComponent, data: { breadcrumb: 'Download Reports Modal'}},
+      { path: 'acceptbidmodal', component: AcceptBidModalComponent , data: { breadcrumb: 'Accept Bid Modal'}},
+      { path: 'eventsoverview', component: EventsOverviewComponent , data: { breadcrumb: 'Events Overview'}},
+      { path: 'eventsetcustomers', component: EventSetCustomersComponent , data: { breadcrumb: 'Event Set Customers'}},
+      { path: 'versionhistory', component: VersionHistoryComponent , data: { breadcrumb: 'Version History'}},
+      { path: 'rejectbidmodal', component: RejectBidModalComponent , data: { breadcrumb: 'Reject Bid Modal'}}
+      
     ]},
 
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PopUpComponent } from './CreateEvent/pop-up/pop-up.component';
-import { PublishEventModalComponent } from './modals/publish-event-modal/publish-event-modal.component';
-import { DownloadReportsModalComponent } from './modals/download-reports-modal/download-reports-modal.component';
-import { AcceptBidModalComponent } from './modals/accept-bid-modal/accept-bid-modal.component';
-import { EventsOverviewComponent } from './createevent/events-overview/events-overview.component';
-import { EventSetCustomersComponent } from './createevent/event-set-customers/event-set-customers.component';
-import { VersionHistoryComponent } from './createevent/version-history/version-history.component';
-import { RejectBidModalComponent } from './modals/reject-bid-modal/reject-bid-modal.component';
+
 
 
 @NgModule({
@@ -19,13 +13,7 @@ import { RejectBidModalComponent } from './modals/reject-bid-modal/reject-bid-mo
     AppComponent,
     LoginComponent,
     PopUpComponent,
-    PublishEventModalComponent,
-    DownloadReportsModalComponent,
-    AcceptBidModalComponent,
-    EventsOverviewComponent,
-    EventSetCustomersComponent,
-    VersionHistoryComponent,
-    RejectBidModalComponent,
+ 
   ],
   imports: [
     BrowserModule,
