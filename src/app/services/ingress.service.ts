ea3d8c432d6a266c17913e8419e729d2f9914381
shekhars271991 <shekhar_suman@outlook.com>
2020-06-14 15:09:57 +0530

    Integrated All Customers and DSO Profile

--- src/app/services/ingress.service.ts
+++ src/app/services/ingress.service.ts
@@ -12,20 +12,30 @@ export class IngressService {
   public currentUser: User = { userId: -1 };
 
   loginUrl = DR_URL + 'loginDSOUser';
+  getDsoDetailsUrl = DR_URL + 'getDsoDetails';
 
   constructor(private httpClient: HttpClient
     , @Inject(LOCAL_STORAGE) private storage: WebStorageService) {
   }
 
   login(email: string, password: string) {
-    console.log('Email : ', email);
-    console.log('Password : ', password);
     var options = {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
     };
     return this.httpClient.post(this.loginUrl
       , { "email": email, "password": password }
+      , options
+    );
+  }
+
+  getDsoDetails(userId: any) {
+    var options = {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+    };
+    return this.httpClient.get(this.getDsoDetailsUrl 
+      + '/' + userId
       , options
     );
   }
