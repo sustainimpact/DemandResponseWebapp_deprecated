b3b8caee2b685828526346e9952cdfd31eefd9ee
shekhar_kony <shekhar.suman@kony.com>
2020-06-15 11:05:36 +0530

    Small fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,3 +4,8 @@ export const environment = {
 
 export const DR_URL = 'http://139.59.30.90:8080/et_dr/rest/';
 export const CUSTOMER_URL = 'http://139.59.30.90:8080/et_dr/';
+
+
+export const UPLOAD = 'UPLOAD';
+export const REUPLOAD = 'REUPLOAD';
+
