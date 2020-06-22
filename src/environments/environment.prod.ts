af835dcef71fd20ecad6ff44f2638b7d3468c9ad
shekhar_kony <shekhar.suman@kony.com>
2020-06-23 00:12:13 +0530

    Adding prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true
 };
 
-export const DR_URL = 'http://139.59.30.90:8080/et_dr/rest/';
-export const CUSTOMER_URL = 'http://139.59.30.90:8080/et_dr/';
+export const DR_URL = 'http://api.energygrid.io:8080/et_dr/rest/';
+export const CUSTOMER_URL = 'http://api.energygrid.io:8080/et_dr/';
 
 
 export const UPLOAD = 'UPLOAD';
